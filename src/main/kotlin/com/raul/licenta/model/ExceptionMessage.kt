afea3112206d770fd6d16b70aed354ca54accda1
laurentiu.pinte <laurentiu.pinte@avelon.com>
2020-07-09 02:44:39 +0300

    error handling

--- src/main/kotlin/com/raul/licenta/model/ExceptionMessage.kt
+++ src/main/kotlin/com/raul/licenta/model/ExceptionMessage.kt
@@ -3,17 +3,17 @@ package com.raul.licenta.model
 import com.fasterxml.jackson.annotation.JsonValue
 
 enum class  ExceptionMessage(val message: String = toString()) {
-    Unknown("UnknownError"),
+    Unknown("Unknown Error"),
     JWTInvalidFormat("JWT has invalid format!"),
-    NoUserWithThatUsername("No user with the provided username was found!"),
+    NoUserWithThatUsername("No user with the provided email was found!"),
     NoUserWithThatId("No user with the provided id was found!"),
     NoImageWithThatId("No image with the provided id was found!"),
     NoCurrentUser("No user was found based on the authentication!"),
     UploadCorrupted("Upload failed due to corrupted data!"),
-    ImageExists("There already is an image like this in the database"),
-    ImageWithNameExists("There already is an image with this name in the database"),
-    UserAlreadyRegistered("There is already an user with this username!"),
-    WrongPassword("Bad credentials!"),
+    ImageExists("There already is an image like this in the database!"),
+    ImageWithNameExists("There already is an image with this name in the database!"),
+    UserAlreadyRegistered("There already is an user with this username!"),
+    WrongPassword("Wrong password or email!"),
     UserNotActivated("User not activated!"),
     AuthorizationCodeExpired("Authorization code expired!");
 
